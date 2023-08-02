015ca67fb8e972870b239804b6c200640587ea41
Juho Hänninen <juho.hanninen@cgi.com>
2023-08-02 17:15:14 +0300

    Process data in preparation script

--- prepare_data.sh
+++ prepare_data.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 echo "Fetching map data..."
-wget -q -O hsl.osm.pbf https://karttapalvelu.storage.hsldev.com/hsl.osm/hsl.osm.pbf
+mkdir -p data/
+wget -q -O data/hsl.osm.pbf https://karttapalvelu.storage.hsldev.com/hsl.osm/hsl.osm.pbf
 
 echo "Data is downloaded!"
+
+node_modules/@project-osrm/osrm/lib/binding/osrm-extract data/hsl.osm.pbf -p node_modules/@project-osrm/osrm/profiles/car.lua
+node_modules/@project-osrm/osrm/lib/binding/osrm-contract data/hsl.osrm
