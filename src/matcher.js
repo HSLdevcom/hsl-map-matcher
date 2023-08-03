58a101418e921b8424f7b5dc945a4187df796b34
Juho Hänninen <juho.hanninen@cgi.com>
2023-08-03 13:52:18 +0300

    Implement matcher module

--- src/matcher.js
+++ src/matcher.js
@@ -1,11 +1,42 @@
+import fs from 'node:fs';
+
 import OSRM from '@project-osrm/osrm';
+
+const DATA_DIRECTORY = './data/';
+
+// Get all available routing dataset names
+const profiles = fs
+  .readdirSync(DATA_DIRECTORY, { withFileTypes: true })
+  .filter((f) => f.isDirectory())
+  .map((dir) => dir.name);
 
 const networks = {};
 
+// Helper function to get available profiles
+const getProfiles = () => Object.keys(networks);
+
 const initNetworks = () => {
-  const osrm = new OSRM();
+  profiles.forEach((profile) => {
+    networks[profile] = new OSRM(`${DATA_DIRECTORY}${profile}/hsl.osrm`);
+  });
 };
 
-const matchGeometry = () => {};
+const matchGeometry = (profile, geometry) => {
+  if (!getProfiles().includes(profile)) {
+    throw Error(`Invalid profile: ${profile}`);
+  }
 
-export default matchGeometry;
+  const osrm = networks[profile];
+
+  osrm.match(
+    { coordinates: geometry, overview: 'full', geometries: 'geojson' },
+    (err, response) => {
+      if (err) {
+        throw err;
+      }
+      return response.matchings[0].geometry;
+    },
+  );
+};
+
+export { initNetworks, getProfiles, matchGeometry };
