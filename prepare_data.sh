89966058f0ac15f792a20571ac46190dd78fb512
e-halinen <54105602+e-halinen@users.noreply.github.com>
2025-07-18 17:22:28 +0300

    Move new data processing to /tmp, improve logging from parent process

--- prepare_data.sh
+++ prepare_data.sh
@@ -1,38 +1,27 @@
 #!/bin/bash
+
+set -Eeuo pipefail
+
 if [ -z $OSM_DATA_URL ]; then
   echo "Error: Env variable OSM_DATA_URL not set. Exiting."
   exit 1
 fi
 
-DATA_DIR='data/'
-BACKUP_DATA_DIR='old_data/'
-OSM_DATA_FILE='map-data.osm.pbf'
+readonly DATA_DIR='data/'
+readonly OSM_DATA_FILE='map-data.osm.pbf'
+readonly OSRM_EXTRACT_PATH='./node_modules/@project-osrm/osrm/lib/binding/osrm-extract'
+readonly OSRM_CONTRACT_PATH='./node_modules/@project-osrm/osrm/lib/binding/osrm-contract'
 
-# Check if there's any existing map data. If there is, back it up
-if [ -f "$DATA_DIR/$OSM_DATA_FILE" ]; then
-  echo "Previous data exists, backing it up."
-  mkdir -p $BACKUP_DATA_DIR
-  cp $DATA_DIR/$OSM_DATA_FILE $BACKUP_DATA_DIR
-  echo "Backup of $DATA_DIR created in $BACKUP_DATA_DIR."
-fi
+tmp_dir="$(mktemp -d)"
+trap 'rm -rf "${tmp_dir}"' EXIT
+NEW_DATA_DIR="${tmp_dir}/${DATA_DIR}"
 
 echo "Fetching new map data..."
-rm -rf $DATA_DIR # Remove previous data before update from main directory
-mkdir -p $DATA_DIR
-if !(curl -v -sSL -o $DATA_DIR/$OSM_DATA_FILE $OSM_DATA_URL); then
-  echo "Error fetching map data from $OSM_DATA_URL. See above output for details. Using previous map data."
-  if [ -f "$BACKUP_DATA_DIR/$OSM_DATA_FILE" ]; then
-    echo "Restoring data from backup..."
-    cp -r $BACKUP_DATA_DIR/$OSM_DATA_FILE $DATA_DIR
-    echo "Backup restored successfully !"
-  else
-    echo "No backup found. Exiting."
-    exit 1
-  fi
-fi
+mkdir -p ${NEW_DATA_DIR}
+curl -v -sSL -o ${NEW_DATA_DIR}/${OSM_DATA_FILE} ${OSM_DATA_URL}
+echo "New map data downloaded!"
 
-echo "Data is downloaded!"
-
+echo "Running OSRM binaries against the downloaded data..."
 for f in osrm-profiles/*.lua
 do
   profile=$(basename $f .lua)
@@ -40,12 +29,16 @@ do
   echo "Processing routing network for profile ${profile}..."
   cp osrm-profiles/${profile}.lua node_modules/@project-osrm/osrm/profiles/${profile}.lua
 
-  mkdir -p data/${profile}/
-  node_modules/@project-osrm/osrm/lib/binding/osrm-extract data/map-data.osm.pbf -p node_modules/@project-osrm/osrm/profiles/${profile}.lua
-  mv data/map-data.osrm* data/${profile}/ # Move data to profile-specific folder
-  node_modules/@project-osrm/osrm/lib/binding/osrm-contract data/${profile}/map-data.osrm
+  mkdir -p ${NEW_DATA_DIR}/${profile}/
+  ${OSRM_EXTRACT_PATH} ${NEW_DATA_DIR}/${OSM_DATA_FILE} -p node_modules/@project-osrm/osrm/profiles/${profile}.lua
+  mv ${NEW_DATA_DIR}/map-data.osrm* ${NEW_DATA_DIR}/${profile}/ # Move data to profile-specific folder
+  ${OSRM_CONTRACT_PATH} ${NEW_DATA_DIR}/${profile}/map-data.osrm
 done
 
 echo "Data preparation ready!"
-exit 0
 
+echo "Replacing previous data..."
+rm -rf "${DATA_DIR}"
+mv "${NEW_DATA_DIR}" "${DATA_DIR}"
+
+exit 0
